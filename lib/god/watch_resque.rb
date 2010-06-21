294f51fc87e802f47c66e46137b8215195b5954f
Jacob Basham <jacob@paperpigeons.net>
2010-06-21 13:34:58 -0500

    Refactored things around a bit

--- lib/god/watch_resque.rb
+++ lib/god/watch_resque.rb
@@ -7,24 +7,28 @@ module God
       extend Forwardable
       def_delegators :@process, :worker_group, :worker_id, :worker_queues, :worker_limit
 
+      def initialize
+        super
+        @process = God::Process::Resque.new
+        self.env ||= {}
+      end
+
       def self.worker
-        God.watch do |w|
-          w.group = 'resque'
-          w.name  = "resque-#{self.worker_group}-#{worker_id}".gsub(/\*/,'star')
-          w.start = "rake resque:work"
-          w.env   = { "QUEUE" => queues }
-          
+        God.task(God::Watch::Resque) do |w|
           yield w
-          
-          # determine the state on startup
-          w.transition(:init, { true => :up, false => :start }) do |on|
+          w.group = 'resque'
+          w.name  = "resque-#{w.worker_group}-#{worker_id}".gsub(/\*/,'star')
+          w.start = "rake resque:work"
+          w.env   = { "QUEUE" => w.worker_queues }
+        
+          transition(:init, { true => :up, false => :start }) do |on|
             on.condition(:process_running) do |c|
               c.running = true
             end
           end
 
           # determine when process has finished starting
-          w.transition([:start, :restart], :up) do |on|
+          transition([:start, :restart], :up) do |on|
             on.condition(:process_running) do |c|
               c.running  = true
             end
@@ -37,34 +41,28 @@ module God
           end
 
           # start if process is not running
-          w.transition(:up, :start) do |on|
+          transition(:up, :start) do |on|
             on.condition(:process_running) do |c|
               c.running = false
             end
           end
 
-          w.transition(:up, :sleeping) do |on|
+          transition(:up, :sleeping) do |on|
             on.condition(:resque_over) do |c|
               c.queues   = w.worker_limits
               c.running  = true
             end
           end
 
-          w.transition(:sleeping, :start) do |on|
+          transition(:sleeping, :start) do |on|
             on.condition(:resque_under) do |c|
               c.queues   = w.worker_limits
               c.running  = false
             end
           end
-          
         end
       end
 
-      def initialize
-        super
-        @process = God::Process::Resque.new
-      end
-    
     end
   end  
 end
