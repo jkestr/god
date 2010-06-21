7dd08fde7f8f1ac72a0160103b6257c713365f3d
Jacob Basham <jacob@paperpigeons.net>
2010-06-21 14:12:45 -0500

    Fix scope error

--- lib/god/watch_resque.rb
+++ lib/god/watch_resque.rb
@@ -22,14 +22,14 @@ module God
           w.start = "rake resque:work"
           w.env   = { "QUEUE" => w.worker_queues }
         
-          transition(:init, { true => :up, false => :start }) do |on|
+          w.transition(:init, { true => :up, false => :start }) do |on|
             on.condition(:process_running) do |c|
               c.running = true
             end
           end
 
           # determine when process has finished starting
-          transition([:start, :restart], :up) do |on|
+          w.transition([:start, :restart], :up) do |on|
             on.condition(:process_running) do |c|
               c.running  = true
             end
@@ -42,20 +42,20 @@ module God
           end
 
           # start if process is not running
-          transition(:up, :start) do |on|
+          w.transition(:up, :start) do |on|
             on.condition(:process_running) do |c|
               c.running = false
             end
           end
 
-          transition(:up, :sleeping) do |on|
+          w.transition(:up, :sleeping) do |on|
             on.condition(:resque_over) do |c|
               c.queues   = w.worker_limits
               c.running  = true
             end
           end
 
-          transition(:sleeping, :start) do |on|
+          w.transition(:sleeping, :start) do |on|
             on.condition(:resque_under) do |c|
               c.queues   = w.worker_limits
               c.running  = false
